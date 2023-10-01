b2c0a6d859883daf48f3b4af636b7f368856e5b1
DESKTOP-PBAOHB5\Administrator <vadimbabkovtc21@gmail.com>
2023-10-01 14:10:40 +0900

    dkd

--- btlm_model_miner.py
+++ btlm_model_miner.py
@@ -79,7 +79,7 @@ def chat():
 
 class ModelMiner():
 
-    def __init__( self, max_length=50, no_repeat_ngram_size=1.5 ):
+    def __init__( self, max_length=50, no_repeat_ngram_size=2 ):
         super( ModelMiner, self ).__init__()
       
     
